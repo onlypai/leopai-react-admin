c776a6f1025027030df5f48ce5a6406f50878684
wangyang <1184024857@qq.com>
2024-09-10 16:43:05 +0800

    feat(hooks): 添加主题和路由菜单相关的hook

---
+++ src/hooks/routeToMenu.tsx
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+import Iconify from '@/components/icons/Iconify';
+
+import { AppRouteObject } from '@/router/types';
+import type { MenuProps } from 'antd';
+
+export type MenuItem = Required<MenuProps>['items'][number] & {
+  children?: MenuItem[];
+};
+
+export function useRouteToMenu() {
+  const routeToMenuFn = useCallback((items: AppRouteObject[]) => {
+    return items.map((item) => {
+      const { meta, children } = item;
+      const menuItem: MenuItem = {
+        key: meta?.key as string,
+        label: meta?.label,
+      };
+
+      const icon = meta?.icon;
+      if (icon) {
+        if (typeof icon === 'string') {
+          menuItem.icon = <Iconify icon={icon} size={24} className="ant-menu-item-icon" />;
+        } else {
+          menuItem.icon = icon;
+        }
+      }
+      if (children) {
+        menuItem.children = routeToMenuFn(children);
+      }
+      return menuItem;
+    });
+  }, []);
+  return routeToMenuFn;
+}
