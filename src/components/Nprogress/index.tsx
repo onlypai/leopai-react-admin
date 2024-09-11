41dfb4069e14cee752afd460ab9f5da7e2bf552a
wangyang <1184024857@qq.com>
2024-09-11 11:30:30 +0800

    feat(progress bar): add nprogress

---
+++ src/components/Nprogress/index.tsx
@@ -0,0 +1,47 @@
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { useEffect, useState } from 'react';
+
+import { useThemeToken } from '@/hooks/themeToken';
+import { useLocation } from 'react-router-dom';
+
+const changeNprogress = (colorPrimary: string) => {
+  const nprogress = document.getElementById('nprogress');
+  if (nprogress) {
+    const bar: HTMLElement = nprogress.querySelector('.bar') as HTMLElement;
+
+    bar.style.background = colorPrimary;
+  }
+};
+
+export default function ProgressBar() {
+  const { pathname } = useLocation();
+  const { colorPrimary } = useThemeToken();
+
+  const [mounted, setMounted] = useState(false); //组件是否挂载
+
+  useEffect(() => {
+    setMounted(true); //组件挂载
+  }, []);
+
+  useEffect(() => {
+    changeNprogress(colorPrimary);
+    //加载进度条
+    NProgress.configure({
+      showSpinner: false,
+    });
+    NProgress.start();
+
+    //边缘情况，确保逻辑严密性
+    if (mounted) {
+      NProgress.done();
+    }
+
+    return () => {
+      NProgress.done();
+    };
+  }, [pathname, mounted, colorPrimary]);
+
+  // 控制进度条的显示、隐藏及样式调整。
+  return null;
+}
