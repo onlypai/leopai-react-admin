c8bf3a60af6bfeae137e27cf323c72723c7239d4
leopai <leopai001@163.com>
2025-12-19 15:13:10 +0800

    feat(flow): The flow supports switching between bright and dark themes

--- src/views/tools/flow/index.tsx
+++ src/views/tools/flow/index.tsx
@@ -1,13 +1,17 @@
 import { memo } from 'react';
 import { Excalidraw } from '@excalidraw/excalidraw';
 import '@excalidraw/excalidraw/index.css';
+import { useAppSelector } from '@/hooks/redux';
+
+import { ETheme } from '@/enum';
 
 //https://docs.excalidraw.com/
 const index = memo(() => {
+  const { theme } = useAppSelector((state) => state.settings);
   return (
     <div>
       <div style={{ height: 'calc(100vh - 105px)' }}>
-        <Excalidraw />
+        <Excalidraw theme={theme === ETheme.Dark ? 'dark' : 'light'} />
       </div>
     </div>
   );
