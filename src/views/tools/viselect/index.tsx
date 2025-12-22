af65184dc21e721e03ee16cf95b8ac63b679502d
leopai <leopai001@163.com>
2025-12-22 15:19:01 +0800

    feat(drag select): The drag-and-drop for selecting Windows folders

---
+++ src/views/tools/viselect/index.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from 'react';
+import SelectionArea, { SelectionEvent } from '@viselect/vanilla';
+import { Tag } from 'antd';
+import { useThemeToken } from '@/hooks/themeToken';
+import { Wrapper } from './style';
+
+export const App = () => {
+  const { colorPrimary } = useThemeToken();
+  const container = useRef<HTMLDivElement>(null);
+  const instance = useRef<SelectionArea | undefined>();
+  const [selected, setSelected] = useState<Set<number>>(() => new Set());
+
+  const extractIds = (els: Element[]): number[] =>
+    els
+      .map((v) => v.getAttribute('data-key'))
+      .filter(Boolean)
+      .map(Number);
+
+  const onStart = ({ event, selection }: SelectionEvent) => {
+    if (!event?.ctrlKey && !event?.metaKey) {
+      selection.clearSelection();
+      setSelected(() => new Set());
+    }
+  };
+
+  const onMove = ({
+    store: {
+      changed: { added, removed },
+    },
+  }: SelectionEvent) => {
+    setSelected((prev) => {
+      const next = new Set(prev);
+      extractIds(added).forEach((id) => next.add(id));
+      extractIds(removed).forEach((id) => next.delete(id));
+      // console.log('选项', next);
+
+      return next;
+    });
+  };
+  const handleBlankClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLElement;
+
+    if (target.closest('.selectable')) return;
+
+    // 左键 + 非组合键
+    if (e.button === 0 && !e.ctrlKey && !e.metaKey) {
+      setSelected(new Set());
+    }
+  };
+
+  useEffect(() => {
+    if (container.current) {
+      instance.current?.destroy();
+      instance.current = new SelectionArea({
+        boundaries: container.current,
+        container: container.current,
+        selectables: '.selectable',
+        // scroll: {
+        //   container: container.current, // 可滚动的容器
+        //   speed: 10, // 滚动速度 px/frame
+        //   margin: 40, // 当鼠标靠近边缘多少 px 开始滚动
+        // },
+        // ...your options
+      });
+
+      instance.current.on('start', onStart);
+      instance.current.on('move', onMove);
+    }
+
+    return () => instance.current?.destroy();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Wrapper $style={{ colorPrimary }}>
+      单选，Ctrl 多选，Shift 连选，拖拽框多选，点击空白取消选择
+      <div ref={container} className="container" onMouseDown={handleBlankClick}>
+        {new Array(20).fill(0).map((_, index) => (
+          <div
+            className={selected.has(index) ? 'selected selectable' : 'selectable'}
+            data-key={index}
+            key={index}
+          >
+            <span>选项下标：{index}</span>
+            <span>2025/12/19 10:30</span>
+            <span>1KB</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        打印选中下标:{' '}
+        {Array.from(selected).map((e) => {
+          return (
+            <Tag color={colorPrimary} key={e}>
+              {e}
+            </Tag>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+export default App;
---
+++ src/views/tools/viselect/style.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import Color from 'color';
+
+export const Wrapper = styled.div<{ $style: { colorPrimary: string } }>`
+  .container {
+    border: 1px solid #ccc;
+    padding: 10px;
+    height: 500px;
+    overflow-y: scroll;
+    margin: 15px 0;
+  }
+  /* 选择区域样式 */
+  .selection-area {
+    background: rgba(46, 115, 252, 0.11);
+    border: 1px solid rgba(98, 155, 255, 0.85);
+    border-radius: 0.15em;
+  }
+
+  /* item */
+  .selectable {
+    padding: 1px 3px;
+    margin-bottom: 5px;
+    user-select: none;
+    /* border: 1px solid rgba(98, 155, 255, 0.85); */
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    transition: all 0.3s;
+    border-radius: 2px;
+    &:hover {
+      background: ${(props) => Color(props.$style.colorPrimary).alpha(0.05).toString()};
+    }
+  }
+
+  /* 选中item */
+  .selected {
+    background: ${(props) => Color(props.$style.colorPrimary).alpha(0.2).toString()};
+    &:hover {
+      background: ${(props) => Color(props.$style.colorPrimary).alpha(0.2).toString()};
+    }
+  }
+`;
